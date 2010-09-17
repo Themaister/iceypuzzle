34a9f99751e217b7e176ca02065b5c64ba01d6ff
Themaister <maister@archlinux.us>
2010-09-17 22:46:09 +0200

    :p

--- movement.hpp
+++ movement.hpp
@@ -5,23 +5,23 @@ namespace Game
 {
    namespace Movement
    {
-      typedef enum Movement
+      enum Movement
       {
          None = 0,
          Up,
          Down,
          Left,
          Right,
-      } Movement;
+      };
    }
 
    namespace Button
    {
-      typedef enum Button
+      enum Button
       {
          None = 0,
          A,
-      } Button;
+      };
    }
 }
 
